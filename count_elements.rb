cf8741566cc212bdd63939f3614a44a4236e6291
github username <github email address>
2018-08-07 19:22:54 -0400

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+letters = ['a','a','a','c','c','d','e','e']
+
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each{|element|
+    if new_hash[element] != nil
+      new_hash[element] += 1
+    else
+      new_hash[element] = 1
+    end
+  }
+  new_hash
 end
  
