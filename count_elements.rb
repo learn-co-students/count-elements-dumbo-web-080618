3ee03d2711345c7138749cc858a77de0d2e0e12b
mikepcarlos <michaelpcarlos@gmail.com>
2018-08-07 16:07:41 -0400

    finished counting

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = Hash.new(0)
+  array.each do |item|
+    new_hash[item] += 1
+  end
+  new_hash
 end
- 
+
+#count_elements(['cat', 'dog', 'fish', 'fish'])
